c668b5dd60e52324f836401f17024fb10d7b6d9e
astrid.widyastiti <astriwidyastiti@gmail.com>
2025-06-26 19:30:52 +0700

    update field email

--- website/forms.py
+++ website/forms.py
@@ -56,7 +56,7 @@ class SignUpForm(UserCreationForm):
 class AddRecordForm(forms.ModelForm):
     first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'First Name', 'class':'form-control'}), label='')
     last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Last Name', 'class':'form-control'}), label='')
-    email = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Email', 'class':'form-control'}), label='')
+    email = forms.EmailField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Email', 'class':'form-control'}), label='')
     phone  = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Phone', 'class':'form-control'}), label='')
     address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Address', 'class':'form-control'}), label='')
     city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'City', 'class':'form-control'}), label='')
