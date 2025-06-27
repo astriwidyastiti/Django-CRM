a4b3c22cf4d4348459f46534f66e97019e21b840
astrid.widyastiti <astriwidyastiti@gmail.com>
2025-06-27 15:16:37 +0700

    update title form

--- website/forms.py
+++ website/forms.py
@@ -2,16 +2,17 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from .models import Record
+from django.core.validators import RegexValidator
 
 class SignUpForm(UserCreationForm):
     email =forms.EmailField(
-        label="",
+        label="Email",
          required=True,
          widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':
         'Email Adress *', 'required': 'required'}),)
-    first_name = forms.CharField(label="",max_length=100, required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':
+    first_name = forms.CharField(label="First Name",max_length=100, required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':
         'First Name *', 'required': 'required'}))
-    last_name = forms.CharField(label="",max_length=100, required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':
+    last_name = forms.CharField(label="Last Name",max_length=100, required=True, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':
         'Last Name *', 'required': 'required'}))
     
     class Meta:
@@ -27,7 +28,7 @@ class SignUpForm(UserCreationForm):
                     'placeholder': 'Username *',
                     'required': 'required',
         })
-        self.fields['username'].label=''
+        self.fields['username'].label='Username'
         self.fields['username'].help_text = '<span class="form-text text-muted"><small>Required. 150 characters or fewe. Letters, digits and @/.+/-/_only.</small></span>'
         
         
@@ -37,7 +38,7 @@ class SignUpForm(UserCreationForm):
                     'placeholder': 'Password *',
                     'required': 'required',
         })
-        self.fields['password1'].label=''
+        self.fields['password1'].label='Password'
         self.fields['password1'].help_text = '<ul class="form-text text-muted"><small><li>Your password can\'t be too similar to your other personal information.</li><li?Your password must contain at least  characters.</li><li>Your password can\'t be a commonlu used password.</li><li>Your password can\'t be entirely numeric</li></ul></span>'
 
         
@@ -49,19 +50,20 @@ class SignUpForm(UserCreationForm):
                     'required': 'required',
         })
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password *'
-        self.fields['password2'].label=''
+        self.fields['password2'].label='Confirm Password'
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification. </small></span>'
         
 
 class AddRecordForm(forms.ModelForm):
-    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'First Name', 'class':'form-control'}), label='')
-    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Last Name', 'class':'form-control'}), label='')
-    email = forms.EmailField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Email', 'class':'form-control'}), label='')
-    phone  = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Phone', 'class':'form-control'}), label='')
-    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Address', 'class':'form-control'}), label='')
-    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'City', 'class':'form-control'}), label='')
-    state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'State', 'class':'form-control'}), label='')
-    zipcode = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Zipcode', 'class':'form-control'}), label='')
+    first_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'First Name', 'class':'form-control'}), label='First Name')
+    last_name = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Last Name', 'class':'form-control'}), label='Last Name')
+    email = forms.EmailField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Email', 'class':'form-control'}), label='Email')
+    phone  = forms.CharField(required=True, validators=[RegexValidator(regex=r'^\d{10,13}$', message="Enter a valid phone number (10–13 digits).")], widget=forms.widgets.TextInput(attrs={'placeholder':'Phone', 'class':'form-control','pattern': '\d{10,13}',
+            'inputmode': 'numeric'}), label='Phone')
+    address = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'Address', 'class':'form-control'}), label='Address')
+    city = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'City', 'class':'form-control'}), label='City')
+    state = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={'placeholder':'State', 'class':'form-control'}), label='State')
+    zipcode = forms.CharField(required=True, validators=[RegexValidator(regex=r'^\d{5}$', message="Zipcode must be 5 digits.")], widget=forms.widgets.TextInput(attrs={'placeholder':'Zipcode', 'class':'form-control', 'maxlength':'5', 'pattern' : '\d{5}', 'inputmode': 'numeric',}), label='Zipcode')
   
     class Meta:
         model = Record
